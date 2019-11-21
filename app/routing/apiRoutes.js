958f43d37aa3613cb0d6aee76b71c44e107edb80
Melissa Knapp <melknapp@gmail.com>
2019-11-21 18:09:05 -0500

    Added questions to survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,16 +5,4 @@ console.log("apiRoutes.js is linked");
 console.log("friendsData is ");
 console.log(friendsData);
 
-//$(document).ready(function(){
-$("#submitButton").on("click", function (event) {
-    event.preventDefault();
-    var bestmatch = "Kathryn";
-    console.log(bestmatch);
-    // $.post("/api/tables", newReservation, function(data) {
-    //   console.log
-    // })
-    $(".modal-body").text(bestmatch);
-    var mySelectVal = parseInt($("#mySelect").val());
-    console.log(typeof (mySelectVal));
-});
-//});
+
