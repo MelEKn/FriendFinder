454eff8083b9b4ddc446896355987bb7c74315e3
Melissa Knapp <melknapp@gmail.com>
2019-11-21 17:27:45 -0500

    Added html pages for home and survey, working on linking js files

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friendsArray = [{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  //photograph from IMDb
+{
+    "name":"Wolfgang",
+    "photo": "https://m.media-amazon.com/images/M/MV5BM2Q0ZDE1ZTItZDE1Mi00ZTg5LTg4ZTYtYzFkMjM4YjM1NDAyXkEyXkFqcGdeQXVyODExNTExMTM@._V1_.jpg",
+    "scores": [
+        3, 
+        1,
+        2, 
+        5, 
+        1, 
+        2, 
+        5,
+        4, 
+        2,
+        4
+    ] 
+}]
+
+module.exports = friendsArray;
