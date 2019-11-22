286728f6c8a9c407bacdbdde7c0d77b8d8d2bd48
Melissa Knapp <melknapp@gmail.com>
2019-11-21 20:23:57 -0500

    Added get and post functions to apiRoutes, can't test yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+//Get the array of possible friends from friends.js
+
 var friendsData = require("../data/friends.js");
 
 console.log("apiRoutes.js is linked");
@@ -5,5 +7,29 @@ console.log("apiRoutes.js is linked");
 console.log("friendsData is ");
 console.log(friendsData);
 
+//Routing
 
-module.exports = function(app) {}
+module.exports = function(app) {
+
+//A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+
+app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+    console.log("friendsData is ");
+    console.log(friendsData);
+    console.log("res is ");
+    console.log(res);
+});
+
+//A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+app.post("/api/friends", function (req, res){
+    console.log("This is from inside the POST route");
+    console.log("req.body is:");
+    console.log(req.body);
+    console.log("res is: ");
+    console.log(res);
+})
+
+
+}
