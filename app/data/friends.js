1e91e79e439cd0e98de92fd6b6cdd893a4c89dcc
Melissa Knapp <melknapp@gmail.com>
2019-11-21 19:34:31 -0500

    built friendsArray of objects to pass between files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,9 @@
-var friendsArray = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
 //photographs from IMDb Sense8 Photo Gallery
 // url: https://www.imdb.com/title/tt2431438/mediaindex?ref_=tt_pv_mi_sm
+
+var friendsArray = [
 {
-    "name": "Wolfgang",
+    "name": "Wolfgang Bogdanow",
     "photo": "https://m.media-amazon.com/images/M/MV5BM2Q0ZDE1ZTItZDE1Mi00ZTg5LTg4ZTYtYzFkMjM4YjM1NDAyXkEyXkFqcGdeQXVyODExNTExMTM@._V1_.jpg",
     "scores": [
         3,
@@ -33,7 +19,7 @@ var friendsArray = [{
     ]
 },
 {
-    "name": "Capheus",
+    "name": "Capheus Onyango",
     "photo": "https://m.media-amazon.com/images/M/MV5BYWZiYzc2MzAtOTkwNC00MjdkLTliY2QtMWZhNmI4YmIwZjU2XkEyXkFqcGdeQXVyODExNTExMTM@._V1_.jpg",
     "scores": [
         1,
@@ -46,6 +32,102 @@ var friendsArray = [{
         1,
         2,
         4
+    ]
+},
+{
+    "name": "Sun Bak",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMjQ0NTYzMjQ0Nl5BMl5BanBnXkFtZTgwNzY1MDcxMjI@._V1_SY1000_CR0,0,1498,1000_AL_.jpg",
+    "scores": [
+        3,
+        3,
+        1,
+        5,
+        5,
+        1,
+        4,
+        2,
+        5,
+        1
+    ]
+},
+{
+    "name": "Nomi Marks",
+    "photo": "https://m.media-amazon.com/images/M/MV5BOTI3MjI1OTgwNV5BMl5BanBnXkFtZTgwMzE3Nzk4NTE@._V1_SY1000_SX1500_AL_.jpg",
+    "scores": [
+        1,
+        5,
+        3,
+        3,
+        3,
+        1,
+        5,
+        1,
+        2,
+        3
+    ]
+},
+{
+    "name": "Kala Dandekar",
+    "photo": "https://m.media-amazon.com/images/M/MV5BNjJmYjVjOGQtMzFiMy00OGU5LWFkOGUtODg5MzFjMGI1Yjg1XkEyXkFqcGdeQXVyODExNTExMTM@._V1_.jpg",
+    "scores": [
+        1,
+        1,
+        2,
+        4,
+        5,
+        2,
+        4,
+        2,
+        2,
+        5
+    ]
+},
+{
+    "name": "Riley Blue",
+    "photo": "https://m.media-amazon.com/images/M/MV5BMjE3MjI0MDUxMl5BMl5BanBnXkFtZTgwMzc1MDcxMjI@._V1_SY1000_CR0,0,1498,1000_AL_.jpg",
+    "scores": [
+        1,
+        3,
+        4,
+        1,
+        2,
+        5,
+        3,
+        1,
+        3,
+        1
+    ]
+},
+{
+    "name": "Lito Rodriguez",
+    "photo": "https://m.media-amazon.com/images/M/MV5BZGRkYTI4YTctZDNiOC00N2I5LTg3ODAtZjMzNmM3YTBiMGQwXkEyXkFqcGdeQXVyODExNTExMTM@._V1_.jpg",
+    "scores": [
+        3,
+        4,
+        1,
+        2,
+        5,
+        3,
+        1,
+        3,
+        1,
+        3
+    ]
+},
+{
+    "name": "Will Gorski",
+    "photo": "https://m.media-amazon.com/images/M/MV5BNDUzMDI4MjQwN15BMl5BanBnXkFtZTgwODE3Nzk4NTE@._V1_SY1000_CR0,0,1500,1000_AL_.jpg",
+    "scores": [
+        4,
+        1,
+        2,
+        5,
+        3,
+        3,
+        1,
+        1,
+        1,
+        3
     ]
 }
 
